fdaaf4fdc742eed049f911e06da4289b618d880d
Jack Palevich <jack.palevich@gmail.com>
2020-02-08 11:31:15 -0800

    Refactor loaders.

--- Sources/FourChan/FourChanLoaders.swift
+++ Sources/FourChan/FourChanLoaders.swift
@@ -3,6 +3,38 @@
 import Combine
 import Foundation
 
+public class Loader<T> : ObservableObject {
+  
+  public var objectWillChange: AnyPublisher<T?, Never> = Publishers.Sequence<[T?], Never>(sequence: []).eraseToAnyPublisher()
+  
+  @Published public var data: T? = nil
+  
+  var cancellable: AnyCancellable?
+  
+  public init(publisher: AnyPublisher<T, Error>) {
+    self.objectWillChange =
+      $data.handleEvents(receiveSubscription: { [weak self] sub in
+        self?.load(publisher:publisher)
+        }, receiveCancel: { [weak self] in
+          self?.cancellable?.cancel()
+      }).eraseToAnyPublisher()
+  }
+  
+  private func load(publisher: AnyPublisher<T, Error>) {
+    if data == nil {
+      cancellable =  publisher
+        .map { Optional($0) }
+        .replaceError(with: nil)
+        .receive(on: RunLoop.main)
+      .assign(to: \Loader.data, on: self)
+    }
+  }
+  
+  deinit {
+    cancellable?.cancel()
+  }
+}
+
 func loader<T: Codable>(endpoint: FourChanAPIEndpoint) -> AnyPublisher<T, Error> {
   URLLoader(url:endpoint.url())
   .decode(type: T.self, decoder: JSONDecoder())
@@ -15,120 +47,22 @@ func fourChanPublisher() -> AnyPublisher<FourChan, Error> {
     .eraseToAnyPublisher()
 }
 
-func chanThreadPublisher(board: BoardName, no: PostNumber) -> AnyPublisher<ChanThread, Error> {
-  loader(endpoint: .thread(board: board, no: no))
-}
 
-func catalogThreadPublisher(board: BoardName) -> AnyPublisher<Catalog, Error> {
-  loader(endpoint: .catalog(board: board))
-}
-
-public class FourChanLoader : ObservableObject {
-  public var objectWillChange: AnyPublisher<FourChan, Never> = Publishers.Sequence<[FourChan], Never>(sequence: []).eraseToAnyPublisher()
-  
-  @Published public var fourChan: FourChan = FourChan()
-  
-  var cancellable: AnyCancellable?
-  
+public class FourChanLoader : Loader<FourChan> {
   public init() {
-    self.objectWillChange =
-      $fourChan.handleEvents(receiveSubscription: { [weak self] sub in
-        self?.load()
-        }, receiveCancel: { [weak self] in
-          self?.cancellable?.cancel()
-      }).eraseToAnyPublisher()
-  }
-  
-  private func load() {
-    if fourChan.categories.count == 0 {
-      cancellable = fourChanPublisher()
-        .receive(on: RunLoop.main)
-        .catch {error in
-          // TODO: Consider reporting the error.
-          Just(FourChan())
-      }
-      .assign(to: \FourChanLoader.fourChan, on: self)
-    }
-  }
-  
-  deinit {
-    cancellable?.cancel()
+    super.init(publisher:fourChanPublisher())
   }
 }
 
-public class ChanThreadLoader : ObservableObject {
-  public let board: BoardName
-  public let no: PostNumber
-  
-  public var objectWillChange: AnyPublisher<ChanThread, Never> = Publishers.Sequence<[ChanThread], Never>(sequence: []).eraseToAnyPublisher()
-  
-  @Published public var thread: ChanThread = ChanThread(posts:[])
-  
-  var cancellable: AnyCancellable?
-  
+public class ChanThreadLoader : Loader<ChanThread> {
   public init(board: BoardName, no: PostNumber) {
-    self.board = board
-    self.no = no
-    
-    self.objectWillChange =
-      $thread.handleEvents(receiveSubscription: { [weak self] sub in
-        self?.load()
-        }, receiveCancel: { [weak self] in
-          self?.cancellable?.cancel()
-      }).eraseToAnyPublisher()
-  }
-  
-  private func load() {
-    if thread.posts.count == 0 {
-      cancellable = chanThreadPublisher(board: self.board, no: self.no)
-        .receive(on: RunLoop.main)
-        .catch {error in
-          // TODO: Consider reporting the error.
-          Just(ChanThread(posts:[]))
-      }
-      .assign(to: \ChanThreadLoader.thread, on: self)
-    }
-  }
-  
-  deinit {
-    cancellable?.cancel()
+    super.init(publisher: loader(endpoint: .thread(board: board, no: no)))
   }
 }
 
-public class CatalogLoader : ObservableObject {
-  public let board: BoardName
-  
-  public var objectWillChange: AnyPublisher<Catalog, Never> = Publishers.Sequence<[Catalog], Never>(sequence: []).eraseToAnyPublisher()
-  
-  @Published public var catalog: Catalog = Catalog()
-  
-  var cancellable: AnyCancellable?
-  
+public class CatalogLoader : Loader<Catalog> {
   public init(board: BoardName) {
-    self.board = board
-    
-    self.objectWillChange =
-      $catalog.handleEvents(receiveSubscription: { [weak self] sub in
-        self?.load()
-        }, receiveCancel: { [weak self] in
-          self?.cancellable?.cancel()
-      }).eraseToAnyPublisher()
-  }
-  
-  private func load() {
-    if catalog.count == 0 {
-      cancellable = catalogThreadPublisher(board: self.board)
-        .receive(on: RunLoop.main)
-        .catch {error in
-          // TODO: Consider reporting the error.
-          Just(Catalog())
-      }
-      .assign(to: \CatalogLoader.catalog, on: self)
-    }
-  }
-  
-  deinit {
-    cancellable?.cancel()
+    super.init(publisher: loader(endpoint: .catalog(board: board)))
   }
 }
 
