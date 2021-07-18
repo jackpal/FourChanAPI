a0e6545065d53b2318dd5fcb163f405ee5b36767
Jack Palevich <jackpal@google.com>
2021-07-18 01:43:22 -0700

    Parse raw URLs
    Add tests for parser.

--- Sources/FourChan/PostTextParser.swift
+++ Sources/FourChan/PostTextParser.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public struct PostTextParser {
-  public enum Element {
+  public enum Element: Equatable {
     case plain(text: String)
     case bold(text: String)
     case strikethrough(text: String)
@@ -9,6 +9,11 @@ public struct PostTextParser {
     case deadLink(text: String)
     case anchor(text: String, href: String)
   }
+
+  /// Matches in-code URLs. See https://gist.github.com/gruber/8891611
+  let urlRegEx = try! NSRegularExpression(
+    pattern: #"(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))"#,
+    options: [])
 
   public init() {}
 
@@ -42,7 +47,23 @@ public struct PostTextParser {
             }
           }
         } else {
-          consumer(.plain(text: text))
+          let nsrange = NSRange(text.startIndex..<text.endIndex, in: text)
+          var lastConsumedIndex = nsrange.lowerBound
+          let matches = urlRegEx.matches(in: text, options: [], range: nsrange)
+          for match in matches {
+            let range = match.range
+            if lastConsumedIndex < range.lowerBound {
+              let prefix = NSRange(location: lastConsumedIndex, length: range.lowerBound - lastConsumedIndex)
+              consumer(.plain(text: String(text[Range(prefix, in: text)!])))
+            }
+            let url = String(text[Range(range, in: text)!])
+            consumer(.anchor(text:url, href: url))
+            lastConsumedIndex = range.upperBound
+          }
+          if lastConsumedIndex < nsrange.upperBound {
+            let prefix = NSRange(location: lastConsumedIndex, length: nsrange.upperBound - lastConsumedIndex)
+            consumer(.plain(text: String(text[Range(prefix, in: text)!])))
+          }
         }
       case .start(let text):
         tagStack.append(text)
---
+++ Tests/FourChanTests/PostTextParserTest.swift
@@ -0,0 +1,86 @@
+import XCTest
+@testable import FourChan
+import Combine
+
+/// Tests that actually hit the real FourChan Servers.
+final class PostTextParserTest: XCTestCase {
+
+  func testPlain() {
+    let result = parse(text: "abc def")
+    XCTAssertEqual(result, [.plain(text: "abc def")])
+  }
+
+  func testBold() {
+    let result = parse(text: "abc<b>def</b>ghi")
+    XCTAssertEqual(result, [
+      .plain(text: "abc"),
+      .bold(text: "def"),
+      .plain(text: "ghi")
+    ])
+  }
+
+  func testStrikethrough() {
+    let result = parse(text: "abc<s>def</s>ghi")
+    XCTAssertEqual(result, [
+      .plain(text: "abc"),
+      .strikethrough(text: "def"),
+      .plain(text: "ghi")
+    ])
+  }
+
+  func testQuote() {
+    let result = parse(text: #"abc<span class="quote">def</span>ghi"#)
+    XCTAssertEqual(result, [
+      .plain(text: "abc"),
+      .quote(text: "def"),
+      .plain(text: "ghi")
+    ])
+  }
+
+  func testDeadlink() {
+    let result = parse(text: #"abc<span class="deadlink">def</span>ghi"#)
+    XCTAssertEqual(result, [
+      .plain(text: "abc"),
+      .deadLink(text: "def"),
+      .plain(text: "ghi")
+    ])
+  }
+
+  func testALink() {
+    let result = parse(text: ##"abc<a href="#foo">def</a>ghi"##)
+    XCTAssertEqual(result, [
+      .plain(text: "abc"),
+      .anchor(text: "def", href: "#foo"),
+      .plain(text: "ghi")
+    ])
+  }
+
+  func testRawLink() {
+    let result = parse(text: ##"abc example.com/a/b.gif http://example.com/e/f.gif ghi"##)
+    let link1 = "example.com/a/b.gif"
+    let link2 = "http://example.com/e/f.gif"
+    XCTAssertEqual(result, [
+      .plain(text: "abc "),
+      .anchor(text: link1, href: link1),
+      .plain(text: " "),
+      .anchor(text: link2, href: link2),
+      .plain(text: " ghi")
+    ])
+  }
+
+  func testEntity() {
+    let result = parse(text: #"&#039;&#044;&amp;&gt;&lt;&quot;"#)
+    XCTAssertEqual(result, [
+      .plain(text: "',&><\""),
+    ])
+  }
+
+  // MARK: Private methods
+  func parse(text: String) -> [PostTextParser.Element] {
+    var accumulator: [PostTextParser.Element] = []
+    PostTextParser().parse(text: text) {
+      accumulator.append($0)
+    }
+    return accumulator
+  }
+}
